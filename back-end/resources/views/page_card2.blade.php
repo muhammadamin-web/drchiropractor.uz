d278f08481f56e6bd7d0d04957305c62d220055e
User <mirboqijonov@example.com>
2023-05-07 08:30:17 +0500

    mmm

--- back-end/resources/views/page_card2.blade.php
+++ back-end/resources/views/page_card2.blade.php
@@ -2,86 +2,86 @@
 
 @section('content')
 <div class="servise_card_page_container">
-        <h1 class="lang" key="servise_card2_page"></h1>
+        <h1 class="lang" key="">{{ __('page_card2.servise_card2_page') }}</h1>
 
-        <h4 class="servise_card1_page_middle_title  lang" key="servise_card2_page_text1"></h4>
+        <h4 class="servise_card1_page_middle_title  lang" key="">{{ __('page_card2.servise_card2_page_text1') }}</h4>
 
-        <p class="servise_card2_page_p_text2 lang" key="servise_card2_page_text2"></p>
+        <p class="servise_card2_page_p_text2 lang" key="">{{ __('page_card2.servise_card2_page_text2') }}</p>
 
-        <h4 class="servise_card1_page_middle_title  lang" key="servise_card2_page_text3"></h4>
+        <h4 class="servise_card1_page_middle_title  lang" key="">{{ __('page_card2.servise_card2_page_text3') }}</h4>
         
         <ul class="servise_card_page_ul">
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li1"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li2"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li3"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li4"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li5"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li6"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li7"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li8"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li9"></li>       
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li1') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li2') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li3') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li4') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li5') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li6') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li7') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li8') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li9') }}</li>       
         </ul>
 
-        <h4 class="servise_card1_page_middle_title  lang" key="servise_card2_page_text4"></h4>
+        <h4 class="servise_card1_page_middle_title  lang" key="">{{ __('page_card2.servise_card2_page_text4') }}</h4>
 
-        <p class="servise_card2_page_p_text2 lang" key="servise_card2_page_text5"></p>
+        <p class="servise_card2_page_p_text2 lang" key="">{{ __('page_card2.servise_card2_page_text5') }}</p>
 
-        <h4 class="servise_card1_page_middle_title  lang" key="servise_card2_page_text6"></h4>
+        <h4 class="servise_card1_page_middle_title  lang" key="">{{ __('page_card2.servise_card2_page_text6') }}</h4>
 
-        <p class="servise_card2_page_p_text2 lang" key="servise_card2_page_text7"></p>
+        <p class="servise_card2_page_p_text2 lang" key="">{{ __('page_card2.servise_card2_page_text7') }}</p>
 
         <ul class="servise_card_page_ul">
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li10"></li> 
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li11"></li> 
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li12"></li> 
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li13"></li> 
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li14"></li> 
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li15"></li> 
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li16"></li> 
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li17"></li> 
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li18"></li>            
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_text10') }}</li> 
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li11') }}</li> 
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li12') }}</li> 
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li13') }}</li> 
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li14') }}</li> 
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li15') }}</li> 
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li16') }}</li> 
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li17') }}</li> 
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li18') }}</li>            
         </ul>
 
-        <p class="servise_card2_page_p_text2 lang" key="servise_card2_page_text8"></p>
+        <p class="servise_card2_page_p_text2 lang" key="">{{ __('page_card2.servise_card2_page_text8') }}</p>
 
-        <h4 class="servise_card1_page_middle_title  lang" key="servise_card2_page_text9"></h4>
+        <h4 class="servise_card1_page_middle_title  lang" key="">{{ __('page_card2.servise_card2_page_text9') }}</h4>
 
         <ul class="servise_card_page_ul">
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li19"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li20"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li21"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li22"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li23"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li24"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li25"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li26"></li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li19') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li20') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li21') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li22') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li23') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li24') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li25') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li26') }}</li>
         </ul>
 
-        <p class="servise_card2_page_p_text2 lang" key="servise_card2_page_text10"></p>
+        <p class="servise_card2_page_p_text2 lang" key="">{{ __('page_card2.servise_card2_page_text10') }}</p>
 
         <ul class="servise_card_page_ul">
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li27"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li28"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li29"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li30"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li31"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li32"></li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li27') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li28') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li29') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li30') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li31') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li32') }}</li>
         </ul>
 
-        <p class="servise_card2_page_p_text2 lang" key="servise_card2_page_text11"></p>
+        <p class="servise_card2_page_p_text2 lang" key="">{{ __('page_card2.servise_card2_page_text11') }}</p>
 
-        <h4 class="servise_card1_page_middle_title  lang" key="servise_card2_page_text12"></h4>
+        <h4 class="servise_card1_page_middle_title  lang" key="">{{ __('page_card2.servise_card2_page_text12') }}</h4>
 
         <ul class="servise_card_page_ul">
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li33"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li34"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li35"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li36"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li37"></li>
-            <li class="servise_card1_page_ul1_li1_text lang" key="servise_card2_page_ul1_li38"></li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li33') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li34') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li35') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="x">{{ __('page_card2.servise_card2_page_ul1_li35') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="x">{{ __('page_card2.servise_card2_page_ul1_li35') }}</li>
+            <li class="servise_card1_page_ul1_li1_text lang" key="">{{ __('page_card2.servise_card2_page_ul1_li38') }}</li>
         </ul>
 
-        <h4 class="servise_card1_page_middle_title  lang" key="servise_card2_page_text13"></h4>
+        <h4 class="servise_card1_page_middle_title  lang" key="">{{ __('page_card2.servise_card2_page_text13') }}</h4>
 
       </div>
       @endsection
